3cce6d913aa9fd655121679477565e304806d828
Nikolay Kastsanev <npkastsanev@gmail.com>
2025-03-22 22:31:17 +0200

    Clean up project

--- server/src/middlewares/cors.js
+++ server/src/middlewares/cors.js
@@ -1,5 +1,5 @@
 export const corsMiddleware = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     
     next();
 };
